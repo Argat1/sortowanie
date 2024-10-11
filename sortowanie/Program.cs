342259aacbe97aab83b69c3becc3fc799ccaadf3
lukasz.jablonski <lukasz.jablonski@S50-14>
2024-10-11 09:42:36 +0200

    added sort

--- sortowanie/Program.cs
+++ sortowanie/Program.cs
@@ -5,6 +5,27 @@
         static void Main(string[] args)
         {
             int [] tab = Losuj();
+
+            for(int i = 0; i < tab.Length-1; i++)
+            {
+                for (int j = 0; j < tab.Length - 1; j++)
+                {
+                    if (tab[j] > tab[j + 1])
+                    {
+                        int x = tab[j];
+                        tab[j] = tab[j + 1];
+                        tab[j + 1] = x;
+                    }
+                }
+            }
+
+            int y = 0;
+            while(y < tab.Length)
+            {
+                Console.Write(tab[y] + ",");
+                y++;
+            }
+
         }
 
         
@@ -16,10 +37,11 @@
             while (i < table.Length)
             {
                 table[i] = r.Next(0, 1001);
-                Console.WriteLine(table[i]);
+                Console.Write(table[i] + ",");
                 i++;
                  
             }
+            Console.WriteLine();
             return table;
         }
     }
