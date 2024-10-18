937330f5e5ec5092f86ade60cd52f615ef8a009b
lukasz.jablonski <lukasz.jablonski@S50-14>
2024-10-18 09:35:42 +0200

    aktualizacja testów

--- TestyJednostkoweBubble/UnitTest1.cs
+++ TestyJednostkoweBubble/UnitTest1.cs
@@ -6,15 +6,15 @@ namespace TestyJednostkoweBubble
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestySprawdzajaceSortowanie()
         {
-
+            //Arenge - przygotuj dane wyjsciowe
             int[] tablica = { 1, 4, 2, 5, 3 };
             int[] tablicadosortowania = { 1, 2, 3, 4, 5 };
-
-            int[] result = SORT.Sortoj(tablica);
-
-            Assert.Equal(tablicadosortowania, result);
+            //Act - wykonaj metode ktore chcesz przestawic
+            int[] wynikoczekiwany = SORT.Sortoj(tablica);
+            //Assert - sprawdz czy wynik metody jest zgodny z oczekiwana wartoscia
+            Assert.Equal(tablicadosortowania, wynikoczekiwany);
         }
     }
 }
